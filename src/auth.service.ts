f9c27aa32f8cd0a0a9ecfe029415ef0939e2119c
kakashijs1 <lickachai22@hotmail.com>
2024-06-25 22:37:40 +0700

    Info Token

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -3,16 +3,16 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
- constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService) {}
 
- async validateUserById(userId: string) {
-   return null;
- }
+  async validateUserById(userId: string) {
+    return null;
+  }
 
- async login(user: any) {
-   const payload = { sub: user.id, username: user.username };
-   return {
-     access_token: this.jwtService.sign(payload),
-   };
- }
+  async login(user: any) {
+    const payload = { sub: user.id, username: user.user };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,71 +1,95 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UnauthorizedException,  } from "@nestjs/common";
-import {  PrismaClient, User } from "@prisma/client";
-import { AuthService } from "./auth.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UnauthorizedException,
+  Headers,
+} from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/client';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 const prisma = new PrismaClient();
 
-@Controller("/api/user")
-export class UserController{
-    constructor(private authService: AuthService) {}
+@Controller('/api/user')
+export class UserController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    @Get("list")
-    async list(){
-        try{
-    return await prisma.user.findMany();
-        } catch (e) {
-            return { status:500, message: e.message }
-        }  
-    }
-
-    @Post("create")
-    async create(@Body() user: User) {
+  @Get('list')
+  async list() {
     try {
-    return await prisma.user.create({ data: user})  
+      return await prisma.user.findMany();
     } catch (e) {
-        return { status:500, message: e.message }
+      return { status: 500, message: e.message };
     }
-    }
+  }
 
-    @Put("edit/:id")
-    async edit(@Body() user: User, @Param("id")id: string) {
-        try{
-        const idValue = parseInt(id);
-    return await prisma.user.update({ data: user, where: {id: idValue}})
+  @Post('create')
+  async create(@Body() user: User) {
+    try {
+      return await prisma.user.create({ data: user });
     } catch (e) {
-        return { status:500, message: e.message }
+      return { status: 500, message: e.message };
     }
+  }
+
+  @Put('edit/:id')
+  async edit(@Body() user: User, @Param('id') id: string) {
+    try {
+      const idValue = parseInt(id);
+      return await prisma.user.update({ data: user, where: { id: idValue } });
+    } catch (e) {
+      return { status: 500, message: e.message };
     }
+  }
 
-    @Delete("remove/:id")
-    async remove(@Param("id") id: string) {
-        try{
-        const idValue = parseInt(id);
-        return await prisma.user.delete({where: { id: idValue }});
-        } catch (e) {
-            return { status:500, message: e.message }
-        }
+  @Delete('remove/:id')
+  async remove(@Param('id') id: string) {
+    try {
+      const idValue = parseInt(id);
+      return await prisma.user.delete({ where: { id: idValue } });
+    } catch (e) {
+      return { status: 500, message: e.message };
     }
+  }
 
-    @Post("login")
-    async login(@Body() user: {usr: string, pwd: string}) {
-        try{
-            const userData = await prisma.user.findFirst({
-                where: {
-                    user: user.usr,
-                    pwd: user.pwd,
-                }
-            });
+  @Post('login')
+  async login(@Body() user: { usr: string; pwd: string }) {
+    try {
+      const userData = await prisma.user.findFirst({
+        where: {
+          user: user.usr,
+          pwd: user.pwd,
+        },
+      });
 
-            if (userData) {
-                const token = await this.authService.login(user);
-                return { token: token };
-              }
-         
-              throw new UnauthorizedException('username or password invalid');
-            } catch (e) {
-              return { message: e.message };
-            }
-          }
-         
+      if (userData) {
+        const token = await this.authService.login(userData);
+        return { token: token };
+      }
+
+      throw new UnauthorizedException('username or password invalid');
+    } catch (e) {
+      return { message: e.message };
+    }
+  }
+
+  @Get('info')
+  async info(@Headers('Authorization') auth: string) {
+    try {
+      const jwt = auth.replace('Bearer', '');
+      const payload = this.jwtService.decode(jwt);
+
+      return { payload: payload };
+    } catch (e) {
+      return { message: e.message };
+    }
+  }
 }
-
